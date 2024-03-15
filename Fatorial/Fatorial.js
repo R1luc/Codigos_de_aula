ca8869e7f11c60c9367366d731a0b7ca88612896
R1luc <ryan.lucas@escolar.ifrn.edu.br>
2024-03-15 07:59:52 -0300

    fatorial dos crias

--- Fatorial/Fatorial.js
+++ Fatorial/Fatorial.js
@@ -1,8 +1,15 @@
 function fatorial(n) {
-    let fat = 1
+    let fat = 1;
     for (let i=2; i<=n; i++)
         fat = fat * i
     return fat;
 }
 
-console.log(fatorial(5));
+function fatorial() {
+    let n = document.getElementById("numero").value;
+    let fat = 1;
+    for (let i=2; i<=n; i++)
+        fat = fat * i
+    document.getElementById("resultado").innerHTML =
+        `Fatorial(${n}) = ${fat}`;
+}
