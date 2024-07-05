b94404f3091fc60f3eb1911660455be428c48ee9
R1luc <ryan.lucas@escolar.ifrn.edu.br>
2024-07-05 08:29:56 -0300

    nomwe

---
+++ Nome Completo/Nome Completo.js
@@ -0,0 +1,6 @@
+function Nome(){
+    let nome = document.getElementById("nome").value 
+    let sobrenome = document.getElementById("sobrenome").value
+    document.getElementById("nome completo").innerHTML =
+        `Seu nome é ${nome} ${sobrenome}`
+}
