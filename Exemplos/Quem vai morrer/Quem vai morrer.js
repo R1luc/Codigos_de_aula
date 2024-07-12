ecb9f55c7fb147884a5aaeec7ef366a54352ac02
R1luc <ryan.lucas@escolar.ifrn.edu.br>
2024-07-12 07:10:30 -0300

    charles

--- Exemplos/Quem vai morrer/Quem vai morrer.js
+++ Exemplos/Quem vai morrer/Quem vai morrer.js
@@ -1,5 +1,5 @@
 function quem(){
-    numero = (Math.floor(Math.random() * 8))
+    numero = (Math.floor(Math.random() * 9))
     grupinho_do_titanic = ["Zazi","Roberto Kennedy","Roberto Pinto","Mohammed","Long Lee","Mariana","Lais","Stefano","Charles"]
     morto = grupinho_do_titanic[numero]
     document.getElementById("morto").innerHTML =
