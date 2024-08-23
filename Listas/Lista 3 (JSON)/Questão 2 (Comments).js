b44191111582c376c0fcbb769512b740406ddb1e
R1luc <ryan.lucas@escolar.ifrn.edu.br>
2024-08-23 08:04:58 -0300

    comment

--- Listas/Lista 3 (JSON)/Questão 2 (Comments).js
+++ Listas/Lista 3 (JSON)/Questão 2 (Comments).js
@@ -1,4 +1,19 @@
-const apiComments = "https://jsonplaceholder.typicode.com/comments"
+const url_posts = 'https://jsonplaceholder.typicode.com/posts';
+fetch(url_posts)
+  .then(response => response.json())
+  .then(posts => list_posts(posts));
+
+function list_posts(posts) {
+  let select = document.getElementById('post');
+  for (let post of posts) {
+    let option = new Option(post.title, post.id);
+    select.options[select.options.length] = option;
+  }
+}
+
+function mostrar_posts() {
+let post_id = document.getElementById('post').value;
+let apiComments = `https://jsonplaceholder.typicode.com/comments?postId=${post_id}`;
 
 fetch(apiComments)
     .then(response => response.json())
@@ -11,7 +26,6 @@ function displayComments(comments){
     
     comments.forEach(comment => {
         const commentElement = document.createElement('div');
-        
         commentElement.innerHTML = `
                 <p>PostID: ${comment.postId}</p>
                 <p>Id: ${comment.id}</p>
@@ -20,12 +34,11 @@ function displayComments(comments){
                 <p>Body: ${comment.body}</p>
             `;
         commentsList.appendChild(commentElement);
-    })
-
-    
-
+        }
+    )
 }
 function displayError(error) {
     const commentList = document.getElementById('comment-list');
     commentList.innerHTML = `<div class="error">Ocorreu um erro: ${error.message}</div>`;
+}
 }
