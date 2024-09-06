fcb098a92ba500e5c2742ad4f70def18e1e67abc
R1luc <ryan.lucas@escolar.ifrn.edu.br>
2024-09-06 08:30:08 -0300

    q2

--- Trabalhos/Copa do Mundo/Questão 2 (Detalhes de um jogo).js
+++ Trabalhos/Copa do Mundo/Questão 2 (Detalhes de um jogo).js
@@ -23,23 +23,21 @@ function displayMatches(matches){
     const matchesList = document.getElementById("match-details")
     matchesList.innerHTML = ""
     
-    matches.forEach(match => {
         const matchElement = document.createElement('div');
         matchElement.innerHTML = `
-                <p>id: ${match.id}</p>
-                <p>"venue": ${match.venue}</p>
-                <p>"location": ${match.location}</p>
-                <p>"status": ${match.status}</p>
-                <p>"attendance": ${match.attendance}</p>
-                <p>"stage_name": ${match.stage_name}</p>
-                <p>"home_team_country": ${match.home_team_country}</p>
-                <p>"away_team_country": ${match.away_team_contry}</p>
-                <p>"datetime": ${match.datetime}</p>
-                <p>"winner": ${match.winner}</p>
-                <p>"winner_code": ${match.winner_code}</p>
+                <p>id: ${matches.id}</p>
+                <p>"venue": ${matches.venue}</p>
+                <p>"location": ${matches.location}</p>
+                <p>"status": ${matches.status}</p>
+                <p>"attendance": ${matches.attendance}</p>
+                <p>"stage_name": ${matches.stage_name}</p>
+                <p>"home_team_country": ${matches.home_team_country}</p>
+                <p>"away_team_country": ${matches.away_team_country}</p>
+                <p>"datetime": ${matches.datetime}</p>
+                <p>"winner": ${matches.winner}</p>
+                <p>"winner_code": ${matches.winner_code}</p>
             `
         matchesList.appendChild(matchElement);
         }
-    )
-}
-}
+      }
+
