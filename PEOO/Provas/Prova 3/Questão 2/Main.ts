06428985cdbf9e9781c46622ad9d90178e113cc0
R1luc <ryan.lucas@escolar.ifrn.edu.br>
2024-12-02 07:11:59 -0300

    a

--- PEOO/Provas/Prova 3/Questão 2/Main.ts
+++ PEOO/Provas/Prova 3/Questão 2/Main.ts
@@ -1,21 +1,17 @@
 import { Filme } from "./Filme";
 import { Catalogo } from "./Catalogo";
 function FiltrarFilmesPorAno(filmes: Filme[], ano: number){
-    let filmesFiltrados = Filme[typeof(Filme)]
     filmes.forEach(filme =>{
         if(filme.pegarAnoLancamento >= ano){
-            filmesFiltrados.push(filme)
+            console.log("- " + filme.pegarTitulo + " " + filme.pegarAnoLancamento)
         }
-    })
-    filmesFiltrados.forEach(filme =>{
-        console.log("- " + filme.pegarTitulo + " " + filme.pegarAnoLancamento)
     })
 }
 
 let filme1 = new Filme("A Origem", "Christopher Nolan", 2010);
 let filme2 = new Filme("Interestelar", "Christopher Nolan", 2014);
 let filme3 = new Filme("Matrix", "Lana e Lilly Wachowski", 1999);
-let filme4 = new Filme("Pulp Fiction: Tempo de Violência","Quentin Tarantino",1994)
+let filme4 = new Filme("Pulp Fiction: Tempo de Violência","Quentin Tarantino", 1994)
 let filme5 = new Filme("Duna","Denis Villeneuve", 2021)
 
 let catalogo = new Catalogo()
