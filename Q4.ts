4eb573fdb3dcc3531e7bf76d2528e640af77d729
Ryan Lucas <137064816+R1luc@users.noreply.github.com>
2026-03-30 17:17:17 -0300

    Add aluno object with student details

---
+++ Q4.ts
@@ -0,0 +1,10 @@
+const aluno = {
+    nome: "Ryan",
+    matricula: "20231144010072",
+    curso: "Informática",
+    IRA: 91.3
+}
+
+console.log(
+    `${aluno.nome} é aluno de ${aluno.curso} com matrícula ${aluno.matricula} e possui IRA ${aluno.IRA}.`
+)
